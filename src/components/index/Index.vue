4684990ab9dc2417b30c1b07ec13b380d606bef2
litthy <litthy@qq.com>
2023-06-10 00:52:59 +0800

    feat: index

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -2,8 +2,19 @@
     <div class="m-navigation">
         <div class="m-list">
             <a class="u-item" :href="item.link" v-for="(item, i) in list" :key="i">
-                <img :src="`${eventLink}${item.img}`" :alt="item.name" />
+                <span class="u-title">{{ item.name }}</span>
+                <el-image class="u-img" :src="`${eventLink}${item.img}`" fit="cover"></el-image>
+                <div class="u-mark"></div>
             </a>
+        </div>
+        <div class="m-mark"> 
+            <div class="mark"></div>
+            <div class="bg"></div> 
+            <div class="left"></div> 
+            <div class="right"></div> 
+            <div class="title">
+                <span>魔盒活动</span>
+            </div>
         </div>
     </div>
 </template>
