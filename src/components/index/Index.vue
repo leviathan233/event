db18b31df368c6e7f372ae42c0be4293b6424615
litthy <litthy@qq.com>
2023-06-13 00:31:00 +0800

    fix: link

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="m-navigation">
         <div class="m-list">
-            <div class="m-list-scroll" :class="{ isLittle }">
-                <a class="u-item" target="_blank" :href="item.link" v-for="(item, i) in list" :key="i">
+            <div class="m-list-scroll" :class="{ isShort }">
+                <a class="u-item" target="_blank" :href="eventLink + item.link" v-for="(item, i) in list" :key="i">
                     <span class="u-title">{{ item.name }}</span>
-                    <el-image class="u-img" :src="`${eventLink}${item.img}`" fit="cover"></el-image>
+                    <el-image class="u-img" :src="`${imgLink}${item.img}`" fit="cover"></el-image>
                     <div class="u-mark"></div>
                 </a>
             </div>
         </div>
         <div class="m-bg">
             <div class="u-mark"></div>
-            <div class="u-dot"></div>  
+            <div class="u-dot"></div>
             <div class="u-left"></div>
             <div class="u-right"></div>
             <div class="u-title">
@@ -22,21 +22,21 @@
 </template>
 
 <script>
-    import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+    import { __imgPath, __Root } from "@jx3box/jx3box-common/data/jx3box.json";
     import { list } from "@/assets/data/index.json";
     export default {
         name: "Index",
         data: function () {
             return {
                 list,
-                imgLink: __imgPath,
+                eventLink: __Root + "event",
             };
         },
         computed: {
-            eventLink() {
+            imgLink() {
                 return __imgPath + "topic/event/img/";
             },
-            isLittle() {
+            isShort() {
                 return this.list.length <= 4;
             },
         },
