4f6a2af8a2e865caa9e897f9430a023bf47ba151
litthy <litthy@qq.com>
2023-06-29 12:45:46 +0800

    add : demo & namespace

---
+++ src/pages/demo/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- 专题页 -->
+    <div class="p-event" :class="'v-' + page_name">
+        <Header :overlayEnable="true"></Header>
+        <router-view></router-view>
+        <!-- <div class="p-event-footer">
+            <div class="wp">
+                <Footer></Footer>
+            </div>
+        </div> -->
+    </div>
+</template>
+
+<script>
+    import { postStat } from "@jx3box/jx3box-common/js/stat";
+    import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+    export default {
+        name: "App",
+        data: function () {
+            return {};
+        },
+        provide: {
+            // TODO: 修改图片对应的文件夹名称
+            __imgRoot: __imgPath + "topic/demo/",
+        },
+        computed: {
+            page_name: function () {
+                return this.$route.name;
+            },
+        },
+        created: function () {
+            // TODO: 修改对应的项目名称
+            postStat("event", "demo");
+        },
+    };
+</script>
---
+++ src/pages/demo/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="p-event-content"></div>
+</template>
+
+<script>
+    export default {
+        name: "Index",
+        inject: ["__imgRoot"],
+        data: function () {
+            return {};
+        },
+
+        methods: {},
+    };
+</script>
+
+<style lang="less">
+    // @import "~@/assets/css/demo/index.less";
+</style>
