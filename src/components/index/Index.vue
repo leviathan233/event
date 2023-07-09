d66ff8b679c65582fd048ccfebb1309b7ac6a217
litthy <litthy@qq.com>
2023-07-09 23:49:52 +0800

    fix: index

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,22 +1,29 @@
 <template>
     <div class="m-navigation">
-        <div class="m-list">
+        <div class="m-dot"></div>
+        <div class="m-mark"></div>
+        <div class="m-footer">
+            <div class="u-left"></div>
+            <div class="u-right"></div>
+        </div>
+        <div class="wp">
+            <img class="u-title" :src="topImg" />
             <div class="m-list-scroll" :class="{ isShort }">
-                <a class="u-item" target="_blank" :href="eventLink + item.link" v-for="(item, i) in list" :key="i">
+                <a
+                    class="u-item"
+                    target="_blank"
+                    :href="eventLink + item.link"
+                    v-for="(item, i) in list"
+                    :key="i"
+                    @mouseover="showName(item.name)"
+                    @mouseout="hideName"
+                >
                     <span class="u-title">{{ item.name }}</span>
                     <el-image class="u-img" :src="`${imgLink}${item.img}`" fit="cover"></el-image>
                     <div class="u-mark"></div>
                 </a>
             </div>
-        </div>
-        <div class="m-bg">
-            <div class="u-mark"></div>
-            <div class="u-dot"></div>
-            <div class="u-left"></div>
-            <div class="u-right"></div>
-            <div class="u-title">
-                <span>魔盒活动</span>
-            </div>
+            <div class="m-name" v-if="show">{{ name }}</div>
         </div>
     </div>
 </template>
@@ -30,17 +37,31 @@
             return {
                 list,
                 eventLink: __Root + "event",
+                show: false,
+                name: "",
             };
         },
         computed: {
             imgLink() {
                 return __imgPath + "topic/event/img/";
             },
+            topImg() {
+                return __imgPath + "topic/event/top.png";
+            },
             isShort() {
                 return this.list.length <= 4;
             },
         },
-        methods: {},
+        methods: {
+            showName(name) {
+                this.show = true;
+                this.name = name;
+            },
+            hideName() {
+                this.show = false;
+                this.name = "";
+            },
+        },
     };
 </script>
 
