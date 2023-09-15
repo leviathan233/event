09562937585ac455bc1e1535a5b29561509ef414
litthy <litthy@qq.com>
2023-09-15 11:39:11 +0800

    fix:style

--- src/pages/blindbox/App.vue
+++ src/pages/blindbox/App.vue
@@ -12,23 +12,23 @@
 </template>
 
 <script>
-    import { postStat } from "@jx3box/jx3box-common/js/stat";
-    import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
-    export default {
-        name: "App",
-        data: function () {
-            return {};
+import { postStat } from "@jx3box/jx3box-common/js/stat";
+import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+export default {
+    name: "App",
+    data: function () {
+        return {};
+    },
+    provide: {
+        __imgRoot: __imgPath + "topic/blindbox/",
+    },
+    computed: {
+        page_name: function () {
+            return this.$route.name;
         },
-        provide: { 
-            __imgRoot: __imgPath + "topic/blindbox/",
-        },
-        computed: {
-            page_name: function () {
-                return this.$route.name;
-            },
-        },
-        created: function () { 
-            postStat("event", "blindbox");
-        },
-    };
+    },
+    created: function () {
+        postStat("event", "blindbox");
+    },
+};
 </script>
--- src/pages/blindbox/Index.vue
+++ src/pages/blindbox/Index.vue
@@ -1,19 +1,46 @@
 <template>
-    <div class="p-event-content"></div>
+    <div class="p-event-blindbox">
+        <el-image :src="`${__imgRoot}bg.png`" class="m-bg" fit="cover" />
+        <div class="p-event-content">
+            <div class="m-content">
+                <div class="m-left">
+                    <div class="logo">
+                        <img :src="`${__imgRoot}logo.png`" alt="魔盒盲盒" />
+                    </div>
+                    <div class="m-box">
+                        <div class="u-box" v-for="item in 10" :key="item">
+                            <img class="u-img" :src="`${__imgRoot}box.png`" alt="奖品" />
+                        </div>
+                    </div>
+                </div>
+                <div class="m-right">
+                    <div class="m-point">9999</div>
+                    <div class="m-prize box">
+                        <h3>
+                            <img :src="`${__imgRoot}prize.png`" alt="奖品一览" />
+                        </h3>
+                        <div class="m-item"></div>
+                    </div>
+                    <div class="m-lottery"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: "Index",
-        inject: ["__imgRoot"],
-        data: function () {
-            return {};
-        },
+export default {
+    name: "Index",
+    inject: ["__imgRoot"],
+    data: function () {
+        return {};
+    },
+    computed: {},
 
-        methods: {},
-    };
+    methods: {},
+};
 </script>
 
 <style lang="less">
-    @import "~@/assets/css/blindbox/index.less";
+@import "~@/assets/css/blindbox/index.less";
 </style>
