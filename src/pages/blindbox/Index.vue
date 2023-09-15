04cb3d0e905dfdab48e04f7bea8db7f817294705
litthy <litthy@qq.com>
2023-09-15 16:53:40 +0800

    fix: style

--- src/pages/blindbox/Index.vue
+++ src/pages/blindbox/Index.vue
@@ -8,20 +8,36 @@
                         <img :src="`${__imgRoot}logo.png`" alt="魔盒盲盒" />
                     </div>
                     <div class="m-box">
-                        <div class="u-box" v-for="item in 10" :key="item">
-                            <img class="u-img" :src="`${__imgRoot}box.png`" alt="奖品" />
+                        <div
+                            :class="['u-box', { active: index === active }]"
+                            v-for="(item, index) in 10"
+                            :key="item"
+                            @click="change(index)"
+                        >
+                            <img class="u-img" :src="`${__imgRoot}box.png`" alt="奖品" v-show="showBox(index)" />
                         </div>
                     </div>
                 </div>
                 <div class="m-right">
                     <div class="m-point">9999</div>
                     <div class="m-prize box">
-                        <h3>
+                        <div class="m-title">
                             <img :src="`${__imgRoot}prize.png`" alt="奖品一览" />
-                        </h3>
+                        </div>
                         <div class="m-item"></div>
                     </div>
-                    <div class="m-lottery"></div>
+                    <div class="m-lottery">
+                        <img :src="`${__imgRoot}refresh.png`" class="u-img refresh" alt="刷新盲盒" />
+                        <img :src="`${__imgRoot}random.png`" class="u-img random" alt="随机开盒" />
+                        <img :src="`${__imgRoot}open.png`" class="u-img open" alt="十连开盒" />
+                        <div class="m-history box u-img" :class="{ history, close }">
+                            <div class="m-title">
+                                <img :src="`${__imgRoot}history.png`" alt="开盒记录" @click="openHistory" />
+                                <img :src="`${__imgRoot}close.png`" class="u-close" alt="关闭" @click="closeHistory" />
+                            </div>
+                            <div class="m-history-content"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -33,11 +49,36 @@ export default {
     name: "Index",
     inject: ["__imgRoot"],
     data: function () {
-        return {};
+        return {
+            history: false,
+            close: false,
+            open: false,
+            all: false,
+            active: "",
+            activeList: [],
+        };
     },
     computed: {},
 
-    methods: {},
+    methods: {
+        showBox(index) {
+            return !this.activeList.includes(index);
+        },
+        change(index) {
+            this.active = ~~index;
+            setTimeout(() => {
+                this.activeList.push(~~index);
+            }, 1800);
+        },
+        openHistory() {
+            this.history = true;
+            this.close = false;
+        },
+        closeHistory() {
+            this.history = false;
+            this.close = true;
+        },
+    },
 };
 </script>
 
